ba062bb227f14dd63449865fd2566c43ef593b79
Vansh <vanshgilhotra8885@gmail.com>
2024-07-14 18:18:17 +0530

    restructures authentication

--- backend/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const userControl = require('../controllers/userControl');
-
-const router = require('express').Router();
-
-router.post('/signup',userControl.signup);
-router.post('refreshtoken',userControl.refreshtoken);
-
-module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const userControl = require('./controllers/userControl');
-require('dotenv').config();
-const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT||5000;
-app.use(express.json());
-app.use(cookieParser);
-
-app.get('/',(req,res)=>{
-    res.json({msg:"This is trial"})
-
-})
-app.listen(PORT,()=>{
-    console.log("Server is Running");
-})
-//routes
-app.use('/user',userControl.signup)
-
-//connecting MONGODB
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            
-        });
-        console.log('MongoDB connected...');
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err.message);
-        process.exit(1);
-    }
-};
 
 connectDB();
+app.use(express.json());
+
+app.use('/api/auth',authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT ,()=>console.log('server running'));
